53a1c01e2a5477e2698d29a8891fa30062c7900e
Priscila Nunes <priscilapereiranunes@gmail.com>
2024-11-13 16:43:08 -0300

    Adiciona rotas CRUD para tabela "produto"

--- sacola/src/server.js
+++ sacola/src/server.js
@@ -1,15 +1,109 @@
 const express = require('express');
+const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
+const connection = mysql.createConnection({
+  host: 'localhost', // substitua pelo seu host
+  user: 'root', // substitua pelo seu usuário
+  password: 'password', // substitua pela sua senha
+  database: 'sacola' // substitua pelo nome do seu banco
+});
+
 app.use(cors()); // habilita cors
 app.use(express.json()); //habilita json como padrão
 
-// Exemplo de rota GET
-app.get('/api/data', (req, res) => {
-  res.json({ message: 'Hello from Express!!!' });
+
+app.post('/api/produto', (req, res) => {
+  const produto = req.body
+  const query = `INSERT INTO produto (nome, codigo, preco, quantidade_disponivel) 
+values ('${produto.nome}', '${produto.codigo}', ${produto.preco}, ${produto.quantidade_disponivel})`
+
+  connection.execute(query, [], (err, results) => {
+    if (err) {
+      console.error('Erro ao inserir o registro:', err);
+      res.status(500).send(`Erro ao inserir o registro. ${err}`);
+      return;
+    }
+
+    res.json({ itens : results});
+  });
 });
+
+app.get('/api/produto', (req, res) => {
+  const query = "SELECT * FROM produto"
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar os registros:', err);
+      res.status(500).send('Erro ao buscar os registros.');
+      return;
+    }
+
+    res.json({ itens: results });
+  });
+});
+
+app.get('/api/produto/:id', (req, res) => {
+  const id = req.params.id
+  const query = `SELECT * FROM produto where id = ${id}`
+
+  connection.execute(query, [], (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar o registro:', err);
+      res.status(500).send('Erro ao buscar o registro.');
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).send('Registro não encontrado.');
+    } else {
+      res.json({itens: results});
+    }
+  });
+});
+
+app.patch('/api/produto/:id', (req, res) => {
+  const id = req.params.id
+  const produto = req.body
+  const query = `UPDATE produto SET nome =  '${produto.nome}', codigo = '${produto.codigo}', preco = ${produto.preco}, quantidade_disponivel = ${produto.quantidade_disponivel} WHERE id = ${id}`
+
+  connection.execute(query, [], (err, results) => {
+    if (err) {
+      console.error('Erro ao atualizar o registro:', err);
+      res.status(500).send(`Erro ao atualizar o registro. ${err}`);
+      return;
+    }
+
+    if (results.affectedRows === 0) {
+      res.status(404).send('Registro não encontrado.');
+    } else {
+      res.json({itens: results});
+    }
+  });
+});
+
+app.delete('/api/produto/:id', (req, res) => {
+  const id = req.params.id
+  const query = `DELETE FROM produto WHERE id = ${id}`
+
+  connection.execute(query, [], (err, results) => {
+    if (err) {
+      console.error('Erro ao deletar o registro:', err);
+      res.status(500).send('Erro ao deletar o registro.');
+      return;
+    }
+
+    if (results.affectedRows === 0) {
+      res.status(404).send('Registro não encontrado.');
+    } else {
+      res.send('Registro deletado com sucesso.');
+    }
+  });
+
+});
+
 
 // Inicializa o servidor backend
 app.listen(PORT, () => {
